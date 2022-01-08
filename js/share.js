4d5764fff9c3f4a57027e25012ef430ea833f434
kim_yeny <des423@naver.com>
2022-01-09 00:51:18 +0900

    Reset

---
+++ js/share.js
@@ -0,0 +1,37 @@
+const url = 'https://test-kkondae.netlify.app';
+
+function setShare() {
+  console.log("setShare 실행");
+
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+
+  const shareTitle = "꼰대력 테스트"
+  const shareDesc = infoList[resultAlt].name;
+  // const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+  // 카카오 API 기본 구성 (수정본)
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDesc,
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
+      },
+    },
+
+    buttons: [
+      {
+        title: '결과 확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL
+        },
+      }
+    ]
+  });
+}
---
+++ js/test.js
@@ -0,0 +1,99 @@
+const main = document.querySelector('#main');
+const qna = document.querySelector('#qna');
+const result = document.querySelector('#result');
+
+const endPoint = 1;
+let yesNum = 0;
+let idx;
+
+// 뒤로 가기
+function backList() {
+    if ( idx <= 0 ) {
+        alert('맨 처음 질문입니다 ｡◕‿◕｡❀');
+        return false;
+    } else {
+        idx -= 1;
+        yesNum -= 1;
+        setList(idx);
+    }
+}
+
+// Result 페이지 세팅
+function setResult(yesNum) {
+    // QnA 사라지고 Result 등장
+    qna.style.WebkitAnimaion = "fadeOut 0.8s"
+    qna.style.animation = "fadeOut 0.8s"
+    setTimeout(() => {
+        result.style.WebkitAnimaion = "fadeIn 0.8s"
+        result.style.animation = "fadeIn 0.8s"
+        setTimeout(() => {
+            qna.style.display = 'none';
+            result.style.display = 'block';
+        }, 350);
+    }, 350);
+
+
+    // 합산한 yesNum 값에 따른 Result
+    let point;
+
+    if (yesNum < 8) {
+        point = 0;
+        console.log("yesNum= ", yesNum);
+        console.log("yesNum < 8 :: point= ", point);
+    } else if (yesNum < 16) {
+        point = 1;
+        console.log("yesNum= ", yesNum);
+        console.log("7 < yesNum < 16 :: point= ", point);
+    } else if (yesNum < 21) {
+        point = 2;
+        console.log("yesNum= ", yesNum);
+        console.log("15 < yesNum :: point= ", point);
+    } else {
+        return false;
+    }
+
+    const resultName = document.querySelector('.resultName');
+    resultName.innerHTML = infoList[point].name;
+
+    const resultDesc = document.querySelector('.resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
+}
+
+// Yes or No 버튼 누를 때마다 값 계산
+function calResult(num) {
+    yesNum += num;
+
+    // 버튼을 눌렀을 때, idx가 마지막이라면 결과페이지로 이동
+    if (idx === endPoint) {
+        setResult(yesNum);
+        return;
+    } else {
+        // idx가 마지막이 아니라면 ++idx 질문 페이지 출력
+        setList(++idx);
+    }
+}
+
+// qna 리스트 세팅
+function setList(idx) {
+    var status = document.querySelector('.statusBar');
+    status.style.width = (95 / endPoint) * idx + '%';
+
+    var q = document.querySelector('.qstnBox');
+    q.innerHTML = qstnList[idx];
+}
+
+// '시작하기' 버튼 클릭
+function start() {
+    main.style.WebkitAnimaion = "fadeOut 0.8s"
+    main.style.animation = "fadeOut 0.8s"
+    setTimeout(() => {
+        qna.style.WebkitAnimaion = "fadeIn 0.8s"
+        qna.style.animation = "fadeIn 0.8s"
+        setTimeout(() => {
+            main.style.display = 'none';
+            qna.style.display = 'block';
+        }, 350);
+        idx = 0;
+        setList(idx);
+    }, 350);
+}
