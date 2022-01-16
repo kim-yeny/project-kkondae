f1fa9f76b253e98b794c102276193329a7029757
kim_yeny <des423@naver.com>
2022-01-16 19:22:10 +0900

    commit

--- resources/js/share.js
+++ resources/js/share.js
@@ -8,7 +8,7 @@ function setShare() {
   const shareDesc = infoList[resultAlt].name;
 
   // const shareImage = url + 'resources/img/result-' + resultAlt + '.png';
-  const shareImage = url + 'resources/img/share.png';
+  const shareImage = url + 'resources/img/thumb.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
 
   // 카카오 API 기본 구성 (수정본)
